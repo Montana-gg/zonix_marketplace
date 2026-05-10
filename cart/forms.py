aed63bb9c6723dd540d4f088e6bd66e9f599817a
Montana-gg <batyrbekt09@gmail.com>
2026-05-10 17:16:38 +0600

    Complete shopping cart: fix detail template and enable add-to-cart forms

--- cart/forms.py
+++ cart/forms.py
@@ -1,7 +1,13 @@
+# cart/forms.py
 from django import forms
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int, label='Количество')
+    quantity = forms.TypedChoiceField(
+        choices=PRODUCT_QUANTITY_CHOICES,
+        coerce=int,
+        initial=1,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
