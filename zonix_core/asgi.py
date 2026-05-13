b3058a768da2504076d4b26abdee608d0d201d43
Montana-gg <batyrbekt09@gmail.com>
2026-05-13 19:46:42 +0600

    Complete rebranding: project renamed to Zonix, DB migrated to zonix_db

--- zonix_core/asgi.py
+++ zonix_core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for velo_core project.
+ASGI config for zonix_core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'velo_core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zonix_core.settings')
 
 application = get_asgi_application()
--- zonix_core/urls.py
+++ zonix_core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for velo_core project.
+URL configuration for zonix_core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
